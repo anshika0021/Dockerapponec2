714fa6f173935ff3ce1fdd4cdae4d33d5dc66bbe
Anshika Sharma <www.anshikasharma5521@gmail.com>
2026-03-18 18:26:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div> 
+      <h1>Welcome to my React app by Sarcastic Ash 😄</h1>
+
+      <form className="form-container">
+        <input 
+          type="text" 
+          placeholder="Enter your name" 
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default App
